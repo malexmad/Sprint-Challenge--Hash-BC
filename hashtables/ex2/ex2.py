c442b3f6384f248910553f3d6d9696aa1024911b
malexmad <malex140@gmail.com>
2020-04-10 12:55:11 -0400

    ex2 test pass

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -11,13 +9,33 @@ class Ticket:
         self.source = source
         self.destination = destination
 
-
+# We can hash each ticket such that the starting location is the key and the destination is the value. Then,
+# when constructing the entire route, the ith location in the route can be found by checking the hash table for the i-1th location.
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = [None] * (length - 1)
 
-    """
-    YOUR CODE HERE
-    """
+    # store ticket in hashtable (key:departure, value:destination)
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    # starting location key is NONE
+    departure = "NONE"
+    ndx = 0
+    while hash_table_retrieve(hashtable, departure) != "NONE":
+        destination = hash_table_retrieve(hashtable, departure)
+        route[ndx] = destination
+        departure = destination
+        ndx += 1
+
+    return route
+
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+#
+# tickets = [ticket_1, ticket_2, ticket_3]
+#
+# # expected = ["PDX", "DCA"]
+# result = reconstruct_trip(tickets, 3)
+# print(result)
